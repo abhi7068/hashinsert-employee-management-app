6f3ae2bfd58cc5e7822cfa272e40e8cd4aca3361
KolliSai <saik98187@gmail.com>
2023-07-03 15:13:46 +0530

    created basic navbar and landing page for website

---
+++ client/src/components/Drawer/index.jsx
@@ -0,0 +1,54 @@
+import Drawer from "@mui/material/Drawer";
+import MenuList from "@mui/material/MenuList";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { styled } from "@mui/material/styles";
+import PeopleIcon from "@mui/icons-material/People";
+import ViewTimelineIcon from "@mui/icons-material/ViewTimeline";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import DraftsIcon from "@mui/icons-material/Drafts";
+const StyledMenuItem = styled(MenuItem)(() => ({
+  "&:hover": {
+    backgroundColor: "#4919f5",
+    color: "white",
+  },
+}));
+
+// eslint-disable-next-line react/prop-types
+export default function TemporaryDrawer({ isOpen, toggleDrawer }) {
+  const list = () => (
+    <MenuList>
+      <StyledMenuItem>
+        <ListItemIcon className=" hover:text-white">
+          <PeopleIcon fontSize="small" />
+        </ListItemIcon>
+        <ListItemText>Employees</ListItemText>
+      </StyledMenuItem>
+      <StyledMenuItem>
+        <ListItemIcon className=" hover:text-white">
+          <ViewTimelineIcon fontSize="small" />
+        </ListItemIcon>
+        <ListItemText>TimeSheet</ListItemText>
+      </StyledMenuItem>
+      <StyledMenuItem>
+        <ListItemIcon className=" hover:text-white">
+          <CalendarMonthIcon fontSize="small" />
+        </ListItemIcon>
+        <ListItemText>CalenderView</ListItemText>
+      </StyledMenuItem>
+      <StyledMenuItem>
+        <ListItemIcon className=" hover:text-white">
+          <DraftsIcon fontSize="small" />
+        </ListItemIcon>
+        <ListItemText>LeaveRequets</ListItemText>
+      </StyledMenuItem>
+    </MenuList>
+  );
+
+  return (
+    <Drawer anchor={"left"} open={isOpen} onClose={toggleDrawer}>
+      {list()}
+    </Drawer>
+  );
+}
---
+++ client/src/components/Navbar/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
+
+import Drawer from "../Drawer/index";
+import Button from "../Button/index";
+
+function Index() {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const toggleDrawer = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <nav className="flex flex-row justify-between py-4 mb-8">
+        <div className="text-text-color">
+          {isOpen ? (
+            <MenuOpenIcon fontSize="large" onClick={toggleDrawer} />
+          ) : (
+            <MenuIcon fontSize="large" onClick={toggleDrawer} />
+          )}
+        </div>
+
+        <div>
+          <Button content="Login" />
+        </div>
+      </nav>
+      <Drawer isOpen={isOpen} toggleDrawer={toggleDrawer} />
+    </>
+  );
+}
+
+export default Index;
