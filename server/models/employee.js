34ff98a37fd50eacfb6efc4ad861de78d1e044ea
BhanuSravya-Nissankararao <121043749+BhanuSravya-Nissankararao@users.noreply.github.com>
2023-07-23 19:16:34 +0530

    Modified Employee

--- server/models/employee.js
+++ server/models/employee.js
@@ -25,10 +25,6 @@ const employeeSchema = new mongoose.Schema(
       type: String,
       required :true
     },
-    imageURL:{
-      type: String,
-      required :true
-    },
     hire_date: {
       type: String,
       required: true,
